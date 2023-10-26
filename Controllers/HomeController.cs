a9f17d35c4d12f667552985362f51be77f09c0f2
dsmv04 <dalton.munoz.v@hotmail.com>
2023-10-26 12:26:29 -0300

    Asignacion 2 dia 3 semana 7

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+using DojoSurvey.Models;
+
+namespace DojoSurveyWithModel.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpPost("process")]
+        public IActionResult ProcessForm(Survey result)
+        {
+            
+            // passing result as view model
+            return View("Result", result);
+        }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
